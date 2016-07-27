daf48c7f13d5be49f9fc37a5004fe5f8b2dd629c
Zihin <411509067@qq.com>
2016-07-27 19:44:26 +0800

    Add QuestionPage

---
+++ Sigma/Main/Me/Question/Controller/SAQuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAQuestionViewController.h
+//  Sigma
+//
+//  Created by Ace Hsieh on 7/27/16.
+//  Copyright © 2016 blackcater. All rights reserved.
+//
+
+#import "SAViewController.h"
+
+@interface SAQuestionViewController : SAViewController
+
+@end
